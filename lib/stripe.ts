a8e11a876a394de2ac8727622beefcccf13299da
snapwave1999-prog <snapwave999@gmail.com>
2026-02-06 12:37:43 -0500

    remove Stripe apiVersion to satisfy types

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,10 +1,11 @@
 // lib/stripe.ts
-import Stripe from 'stripe'
+import Stripe from 'stripe';
 
 if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error('STRIPE_SECRET_KEY is not set')
+  throw new Error('STRIPE_SECRET_KEY is not set');
 }
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-10-16',
-})
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+
+
