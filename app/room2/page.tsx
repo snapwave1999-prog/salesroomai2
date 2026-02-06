78fd90ff07939751d219b39e2fca2b3a1b94f86d
snapwave1999-prog <snapwave999@gmail.com>
2026-02-06 12:20:48 -0500

    fix RoomClient import and props

--- app/room2/page.tsx
+++ app/room2/page.tsx
@@ -1,11 +1,13 @@
 // app/room2/page.tsx
-import { RoomClient, type RoomClientProps } from './room-client';
+import { RoomClient } from './room-client';
 
 export default function Room2Page() {
-  const pitchId: RoomClientProps['pitchId'] = 1; // ou la vraie valeur
+  const pitchId = 1; // remplace par la vraie valeur si nécessaire
 
   return <RoomClient pitchId={pitchId} />;
 }
+
+
 
 
 
--- app/room2/room-client.tsx
+++ app/room2/room-client.tsx
@@ -1,20 +1,21 @@
 'use client';
 
-type RoomClientProps = {
+export type RoomClientProps = {
   pitchId: number;
 };
 
-export function RoomClient({ pitchId }: RoomClientProps) {
-  // TODO: garde ici tout ton code actuel de RoomClient
-  // utilise pitchId comme avant
+export function RoomClient(props: RoomClientProps) {
+  const { pitchId } = props;
 
   return (
     <div>
-      {/* Exemple minimal, remplace par ton vrai contenu */}
       <p>Room client pour le pitch #{pitchId}</p>
     </div>
   );
 }
+
+
+
 
 
 
